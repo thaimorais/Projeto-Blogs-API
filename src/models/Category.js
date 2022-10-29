d62c700be1d496fa82ef59a490d72b7f2e9b0c43
Thaísy <thaisy-ms@outlook.com>
2022-10-28 23:31:39 -0300

    req07

---
+++ src/models/Category.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    tableName: 'categories',
+    timestamps: false,
+  });
+
+  return Category;
+};
